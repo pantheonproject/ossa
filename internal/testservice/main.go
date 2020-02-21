72275f92579ab3700b17d7eb281fd40eeb13e393
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-02-21 23:11:23 +1100

    Use stream and unary interceptors from ossa

--- internal/testservice/main.go
+++ internal/testservice/main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"time"
 
+	"github.com/pantheonproject/ossa"
 	"google.golang.org/grpc"
 )
 
+var bindAddress = "localhost:6687"
+
 func main() {
 	// Create the server
-	gserver := grpc.NewServer()
+	gserver := grpc.NewServer(grpc.UnaryInterceptor(ossa.UnaryIntercept), grpc.StreamInterceptor(ossa.StreamIntercept))
 	s := NewServer(gserver)
-	lis, err := net.Listen("tcp", "localhost:6687")
+	lis, err := net.Listen("tcp", bindAddress)
 	if err != nil {
 		log.Fatalf("error starting net listener: %v", err)
 	}
-	err = s.Serve(lis)
+	go serverStart(s, lis)
+	time.Sleep(2 * time.Second)
+	clientConnect(bindAddress)
+}
+
+func clientConnect(address string) {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("error dialing client connection: %v", err)
+	}
+	client := NewTestClient(conn)
+	ctx := context.Background()
+	res, err := client.AddThing(ctx, &AddThingRequest{})
+	if err != nil {
+		log.Fatalf("error adding thing: %v", err)
+	}
+	log.Printf("response: %v\n", res)
+}
+
+func serverStart(s *Server, lis net.Listener) {
+	err := s.Serve(lis)
 	if err != nil {
 		log.Fatalf("error starting server: %v", err)
 	}
+	log.Println("exited server with no error")
 }
--- internal/testservice/server.go
+++ internal/testservice/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // Server is a Test server
@@ -24,4 +27,9 @@ func NewServer(server *grpc.Server) *Server {
 // Serve starts the server
 func (s *Server) Serve(lis net.Listener) error {
 	return s.server.Serve(lis)
+}
+
+// AddThing adds a thing
+func (s *Server) AddThing(ctx context.Context, req *AddThingRequest) (*AddThingResponse, error) {
+	return &AddThingResponse{}, status.Error(codes.Canceled, "test")
 }
