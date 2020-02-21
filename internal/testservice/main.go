230a21ba3944f16b8382de265170b8a3338a1711
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-02-21 22:39:14 +1100

    Start grpc server in test client

--- internal/testservice/main.go
+++ internal/testservice/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 func main() {
 	// Create the server
-	NewServer()
+	gserver := grpc.NewServer()
+	s := NewServer(gserver)
+	lis, err := net.Listen("tcp", "localhost:6687")
+	if err != nil {
+		log.Fatalf("error starting net listener: %v", err)
+	}
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("error starting server: %v", err)
+	}
 }
--- internal/testservice/server.go
+++ internal/testservice/server.go
@@ -1,12 +1,27 @@
 package main
 
+import (
+	"net"
+
+	"google.golang.org/grpc"
+)
+
 // Server is a Test server
 type Server struct {
 	UnimplementedTestServer
+	server *grpc.Server
 }
 
 // NewServer returns a new Server
-func NewServer() *Server {
-	s := &Server{}
+func NewServer(server *grpc.Server) *Server {
+	s := &Server{
+		server: server,
+	}
+	RegisterTestServer(server, s)
 	return s
+}
+
+// Serve starts the server
+func (s *Server) Serve(lis net.Listener) error {
+	return s.server.Serve(lis)
 }
